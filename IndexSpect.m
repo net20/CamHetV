b990e7a8a20fe3db58f11664660be54bea77440f
Nicholas Taylor <net20@cam.ac.uk>
2020-10-23 13:01:32 +0100

    Switch to column-wise norm for spectrogram vis

--- IndexSpect.m
+++ IndexSpect.m
@@ -1,10 +1,20 @@
 function [ IndexOut ] = IndexSpect( AmpSpect, nLevels )
-%INDEXSPECT Summary of this function goes here
-%   Detailed explanation goes here
-    ScaleFact = range(log(AmpSpect(AmpSpect>0)));
-    ScaleOffs = min(log(AmpSpect(AmpSpect>0)));
+%INDEXSPECT Convert an amplitude spectrum from double to int
+%   AmpSpect is an amplitude or power spectrogram, at some
+%   floating-point precision (it'll be double, everything in Matlab's a
+%   double).  Displaying that directly using imagesc is a bit slow,
+%   particularly when zooming and panning.  So this takes logs and
+%   quantizes the log spectrogram into nLevels levels, suitable for
+%   displaying using image, indexed.  Then squashes it down into the
+%   smallest integer type it'll fit.
+
+    ampMin = min(AmpSpect(AmpSpect>0));
+    AmpSpect(AmpSpect == 0) = ampMin;
+    logSpect = log(AmpSpect);
+    ScaleFacts = range(logSpect);
+    ScaleOffs = min(logSpect);
     
-    DispP = (((log(AmpSpect)-ScaleOffs)/ScaleFact)*(nLevels - 1)) + 1;
+    DispP = (((logSpect - ScaleOffs) ./ ScaleFacts) * nLevels-1) + 1;
     
     if (nLevels < 256)
     	IndexOut = uint8(DispP);
